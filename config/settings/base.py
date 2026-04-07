882f98e348d57145901bd89f4e0414831870290c
Jhonbriandev <jhonrw20@gmail.com>
2026-04-06 21:24:48 -0500

    feat; Add schema DRF

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,6 +175,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    # Agregar schema de DRF
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
